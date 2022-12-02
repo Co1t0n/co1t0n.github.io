d8667f827db578f35fa9b33f39faf977f1cbdd8b
Co1t0n <92403275+Co1t0n@users.noreply.github.com>
2022-12-02 14:43:58 -0500

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-{
-  "userAgent": "fx", // Firefox userAgent Spoof
-  "prefix": "/service/", // Proxy Prefix
-  "wss": true, // WebSocket Server
-  "corrosion": [false, {}], // Corrosion Dual-Server
-  "title": "Rhodium", // Page Title on Proxied Pages
-  "server": http.Server(), // Your Http Server
-  "encode": "xor" // Encoding for Proxy URLS
-}
